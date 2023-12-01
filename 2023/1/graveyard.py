d4dc1fec0dd1fc5a6047c743fb77dce1da22ffe9
Claire Fletcher <claire.fletcher@pexip.com>
2023-12-01 17:02:25 +0000

    simplified and combined method. Still need to consider reduced code duplication for the word substituting reversed method.

--- 2023/1/graveyard.py
+++ 2023/1/graveyard.py
@@ -11,3 +11,20 @@ def find_all_digits(line):
         generator: A generator that yields the digits found in the line.
     """
     return filter(str.isdigit, line)
+
+
+def find_last_digit(line):
+    concat = ""
+
+    for char in line:
+        # If it is a number, then return!!
+        if char.isdigit():
+            return char
+        else:
+        # Concatenate the string until a number matches the regex
+            concat += char
+            number = re.findall(r'(?=(orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)\b)\w+',
+                concat, flags=re.IGNORECASE)
+            if number.__len__() == 1:
+                return words_to_numbers[number[0][::-1]] # Reverse the string
+    return None
--- 2023/1/main.py
+++ 2023/1/main.py
@@ -17,50 +17,56 @@ words_to_numbers = {
 def read_file_into_matrix(file_name):
     matrix = []
     with open(file_name, 'r') as file:
+
         for line in file:
             row = list(line.strip())
             matrix.append(row)
+
     return matrix
 
 ## Find Digit - Part A
 def find_digit(line):
     for char in line:
+
         if char.isdigit():
             return char
+
     return None
 
 ## Find Digit - Part B
-def find_first_digit(line):
+def match_num_pattern(concat):
+    pattern = r'(?=(zero|one|two|three|four|five|six|seven|eight|nine)\b)\w+'
 
-    grepped_string = ""
+    return re.findall(pattern, concat, flags=re.IGNORECASE)
 
-    for char in line:
-        # If it is a number, then return!!
-        if char.isdigit():
-            return char
-        else:
-        # Concatenate the string until a number matches the regex
-            grepped_string += char
-            number = re.findall(r'(?=(zero|one|two|three|four|five|six|seven|eight|nine)\b)\w+',
-                grepped_string, flags=re.IGNORECASE)
-            if number.__len__() == 1:
-                return words_to_numbers[number[0]]
-    return None
+def match_num_pattern_reversed(concat):
+    pattern = r'(?=(orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)\b)\w+'
 
-def find_last_digit(line):
-    grepped_string = ""
+    return re.findall(pattern, concat, flags=re.IGNORECASE)
+
+def find_digit_words(line, reversed):
+    concat = ""
 
     for char in line:
-        # If it is a number, then return!!
+        # If it is a number already, then return!
         if char.isdigit():
             return char
+
         else:
-        # Concatenate the string until a number matches the regex
-            grepped_string += char
-            number = re.findall(r'(?=(orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)\b)\w+',
-                grepped_string, flags=re.IGNORECASE)
-            if number.__len__() == 1:
-                return words_to_numbers[number[0][::-1]]
+            # Concatenate the string until a number matches the regex
+            concat += char
+            # Use the correct pattern matching
+            if reversed:
+                number = match_num_pattern_reversed(concat)
+                # If we have found a number then stop looking!
+                if number.__len__() == 1:
+                    return words_to_numbers[number[0][::-1]]
+            else:
+                number = match_num_pattern(concat)
+                # If we have found a number then stop looking!
+                if number.__len__() == 1:
+                    return words_to_numbers[number[0]]
+
     return None
 
 def decode_calibration_value(first_digit, last_digit):
@@ -70,21 +76,31 @@ def calculate_summed_calibration_value(calibration_values):
     sum = 0
     for i in calibration_values:
         sum = sum + i
+
     return sum
 
 def main():
     print("Reading input file...")
-    matrix = read_file_into_matrix("input.txt")
+    matrix = read_file_into_matrix("/home/claire/claire-fletcher/advent-of-code/2023/1/input.txt")
 
     print("Calulating first and last digits...")
     calibration_values_a, calibration_values_b = [], []
     for line in matrix:
+        print("new!")
         backwards = list(reversed(line))
-        calibration_values_a.append(decode_calibration_value(find_digit(line), find_digit(backwards)))
-        calibration_values_b.append(decode_calibration_value(find_first_digit(line), find_last_digit(backwards)))
 
-    print("Final Result Part A: ", calculate_summed_calibration_value(calibration_values_a))
-    print("Final Result Part B: ", calculate_summed_calibration_value(calibration_values_b))
+        calibration_values_a.append(
+            decode_calibration_value(find_digit(line),
+            find_digit(backwards)))
+
+        calibration_values_b.append(
+            decode_calibration_value(find_digit_words(line, False),
+            find_digit_words(backwards, True)))
+
+    print("Final Result Part A: ",
+        calculate_summed_calibration_value(calibration_values_a))
+    print("Final Result Part B: ",
+        calculate_summed_calibration_value(calibration_values_b))
 
 if __name__ == "__main__":
     main()

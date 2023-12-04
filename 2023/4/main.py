b2c36c22b084614a004b17eba42c233ab7ae60af
Claire Fletcher <claire.fletcher@pexip.com>
2023-12-04 12:49:58 +0000

    add working part B which can be looked at for improvements

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -6,11 +6,14 @@ import re
 # Read in the input
 cards = [line.strip().split(": ")[1:] for line in open("input.txt", "r")]
 
-# Total points overall
-points = 0
+# Total Part A points overall
+part_a_points = 0
 
-for card in cards:
-    # Read each card in as NUMBERS into two arrays/lists separate the string by |
+# Part B list of number of copies of each card
+num_cards = [1] * len(cards)
+
+for i, card in enumerate(cards):
+    # Read card in as NUMBERS into two arrays/lists separate the string by |
     winning_numbers, scratch_numbers = card[0].split("|")
     winning_numbers = [int(n) for n in re.findall(r"\d+", winning_numbers)]
     scratch_numbers = [int(n) for n in re.findall(r"\d+", scratch_numbers)]
@@ -18,8 +21,15 @@ for card in cards:
     # Find repeats
     points_set = set(scratch_numbers).intersection(winning_numbers)
 
+    # If there is a win
     if points_set != set():
-        points += 2 ** (len(points_set)-1)
+        part_a_points += 2 ** (len(points_set)-1)
 
+        # Part B if there is a win
+        for _ in range(num_cards[i]):
+            # Add to copies of cards
+            for index in range(1, len(points_set)+1):
+                num_cards[i+index] += 1 # Add a copy to the correct card
 
-print("Part A: ", points)
+print("Part A: ", part_a_points)
+print("Part B: ", sum(num_cards))

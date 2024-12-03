33ba19ca72c31284fa9201ac363493fa27c87a7a
Claire Fletcher <claire.fletcher@kaluza.com>
2024-12-03 17:13:47 +0000

    feat(day1): initial day 1 solutions without library or use of test suite yet

--- 2024/1/main-2.py
+++ 2024/1/main-2.py
@@ -10,25 +10,36 @@ def read_file_into_arrays(file_name):
             array2.append(int(row[1]))
     return array1, array2
 
-def find_distance(location1, location2):
-    return abs(location1 - location2)
-
 def sort_array(array):
     return sorted(array)
 
+def array_to_dict(array):
+    dict = {}
+    for i in array:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    return dict
+
+def calculate_similarity(left, dict):
+    total = 0
+    for i in left:
+        if i in dict:
+            total += i * dict[i]
+    return total
+
 def main():
     print("Reading input file...")
-    left_list, right_list = read_file_into_arrays("input.txt")
+    left_list, right_list = read_file_into_arrays("test-input.txt")
     sorted_left = sort_array(left_list)
     sorted_right = sort_array(right_list)
 
-    # for each in sorted left and right, calculate difference and add to total
-    total = 0
-    for i in range(len(sorted_left)):
-        total += find_distance(sorted_left[i], sorted_right[i])
+    similarity_key = array_to_dict(sorted_right)
+    total = calculate_similarity(sorted_left, similarity_key)
 
     print(
-        "Final Result Part A: ",
+        "Final Result Part B: ",
         total
     )
 
